9f941e529035c687f5e146c87775dccd4c1ce605
Martin Renteria <renteriamp96@gmail.com>
2020-12-12 11:35:14 -0800

    Able to create html file with manager information with current code

--- Develop/app.js
+++ Develop/app.js
@@ -13,35 +13,69 @@ const render = require("./lib/htmlRenderer");
 const teamInput = [];
 
 //Manager information template
-function ManagerInfo() {
+function managerInfo() {
     
-        return inquirer.prompt([{
-            type: "input",
-            message: "Enter Manager's name",
-            name: "name",
-        }, {
-            type: "input",
-            message: "Enter Manager's ID?",
-            name: "id",
-        }, {
-            type: "input",
-            message: "Enter manager's email?",
-            name: "email",
-        }, {
-            type: "input",
-            message: "Enter manager's number?",
-            name: "officeNumber",
-        }]).then((managerInput) => {
+    return inquirer.prompt([{
+        type: "input",
+        message: "Enter Manager's name",
+        name: "name",
+    }, {
+        type: "input",
+        message: "Enter Manager's ID?",
+        name: "id",
+    }, {
+        type: "input",
+        message: "Enter Manager's email?",
+        name: "email",
+    }, {
+        type: "input",
+        message: "Enter Manager's number?",
+        name: "officeNumber",
+    }]).then((managerInput) => {
+
+        const newManager = new Manager(managerInput.name, managerInput.id, managerInput.email, managerInput.officeNumber);
+        teamInput.push(newManager);
+        employeeInput();
+
+    });
+
+}
+
+
+// Starting page
+function employeeInput() {
+
+    return inquirer.prompt([{
+        type: "list",
+        name: "addEmployee",
+        message: "Which type of employee would you like to add?",
+        choices: [{ name: "Engineer", value: 0 }, { name: "Intern", value: 1 }, { name: "Manager", value: 2 }, { name: "I don't want to add any more team members", value: 3 }],
+    }]).then((employeeType) => {
+        //if they selected engineer, ask engineer questions
+        if (employeeType.newEmployee === 0) {
+            engineerInfo();
+            //else, ask Intern questions
+        } else if (employeeType.newEmployee === 1) {
+            internInfo();
+            // else, ask Manager questions
+        } else if (employeeType.newEmployee === 2) {
+            managerInfo();
+            //exit application
+        } else {
+            createHtmlFile();
+        }
+    });
+}
+
+    function createHtmlFile() {
+
+        const employeeTeamPage = render(teamInput);
     
-            const newManager = new Manager(managerInput.name, managerInput.id, managerInput.email, managerInput.officeNumber);
-            teamInput.push(newManager);
-            ManagerInfo();
-    
+        fs.writeFile("output.html", employeeTeamPage, (err) => {
+            if (err) console.log("Unable to write file");
+            else console.log("File was written successfully");
         });
     
     }
 
-    ManagerInfo();
-
-
-    
+    managerInfo();
